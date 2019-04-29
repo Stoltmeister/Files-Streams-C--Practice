45f30092fb178ffe94222b8f37e813a2559b5bd5
Josh Stoltenberg <Josh.Stoltenberg@myhealthchampion.com>
2019-04-29 13:35:08 -0500

    Dictionary version implemented

--- Program.cs
+++ Program.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace DataProcessor
 {
     class Program
     {
+        private static ConcurrentDictionary<string, string> FilesToProcess =
+            new ConcurrentDictionary<string, string>();
+
         static void Main(string[] args)
         {
             WriteLine("Parsing command line options");
@@ -25,6 +30,7 @@ namespace DataProcessor
                 WriteLine($"Watching directory {directoryToWatch} for changes");
 
                 using (var inputFileWatcher = new FileSystemWatcher(directoryToWatch))
+                using (var timer = new Timer(ProcessFiles, null, 0, 1000))
                 {
                     inputFileWatcher.IncludeSubdirectories = false;
                     inputFileWatcher.InternalBufferSize = 32768; // 32 KB
@@ -43,17 +49,26 @@ namespace DataProcessor
                     ReadLine();
                 }
             }
-
         }
 
         private static void FileCreated(object sender, FileSystemEventArgs e)
         {
             WriteLine($"* File created: {e.Name} - type: {e.ChangeType}");
+
+            //var fileProcessor = new FileProcessor(e.FullPath);
+            //fileProcessor.Process();
+
+            FilesToProcess.TryAdd(e.FullPath, e.FullPath);
         }
 
         private static void FileChanged(object sender, FileSystemEventArgs e)
         {
             WriteLine($"* File changed: {e.Name} - type: {e.ChangeType}");
+
+            //var fileProcessor = new FileProcessor(e.FullPath);
+            //fileProcessor.Process();
+
+            FilesToProcess.TryAdd(e.FullPath, e.FullPath);
         }
 
         private static void FileDeleted(object sender, FileSystemEventArgs e)
@@ -68,33 +83,19 @@ namespace DataProcessor
 
         private static void WatcherError(object sender, ErrorEventArgs e)
         {
-            WriteLine($"ERROR: file system watching may no longer be active:  {e.GetException()}");
+            WriteLine($"ERROR: file system watching may no longer be active: {e.GetException()}");
         }
 
-        private static void ProcessDirectory(string directoryPath, string fileType)
+        private static void ProcessFiles(Object stateInfo)
         {
-            //var allFiles = Directory.GetFiles(directoryPath); // to get all files
-
-            switch (fileType)
+            foreach (var fileName in FilesToProcess.Keys) // May not be in order of adding
             {
-                case "TEXT":
-                    string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
-                    foreach (var textFilePath in textFiles)
-                    {
-                        var fileProcessor = new FileProcessor(textFilePath);
-                        fileProcessor.Process();
-                    }
-                    break;
-                default:
-                    WriteLine($"ERROR: {fileType} is not supported");
-                    return;
+                if (FilesToProcess.TryRemove(fileName, out _))
+                {
+                    var fileProcessor = new FileProcessor(fileName);
+                    fileProcessor.Process();
+                }
             }
-        }
-
-        private static void ProcessSingleFile(string filePath)
-        {
-            var fileProcessor = new FileProcessor(filePath);
-            fileProcessor.Process();
         }
     }
 }
