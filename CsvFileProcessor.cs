4e0a5c91483578967cf9b595a869e7adb88f1778
Josh Stoltenberg <Josh.Stoltenberg@myhealthchampion.com>
2019-05-07 13:57:23 -0500

    Extra functionality for CSV files explored

--- CsvFileProcessor.cs
+++ CsvFileProcessor.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace DataProcessor
 {
@@ -22,20 +23,36 @@ namespace DataProcessor
         {
             using (StreamReader input = File.OpenText(InputFilePath))
             using (CsvReader csvReader = new CsvReader(input))
+            using (StreamWriter output = File.CreateText(OutputFilePath))
+            using (var csvWriter = new CsvWriter(output))
             {
-                IEnumerable<dynamic> records = csvReader.GetRecords<dynamic>();
+                IEnumerable<ProcessedOrder> records = csvReader.GetRecords<ProcessedOrder>();
 
                 csvReader.Configuration.TrimOptions = TrimOptions.Trim;
                 csvReader.Configuration.Comment = '@'; // Default is #
                 csvReader.Configuration.AllowComments = true;
+                csvReader.Configuration.RegisterClassMap<ProcessedOrderMap>();
 
-                foreach (var record in records)
+                //csvWriter.WriteRecords(records);
+
+                csvWriter.WriteHeader<ProcessedOrder>();
+                csvWriter.NextRecord();
+
+                var recordsArray = records.ToArray();
+                for (int i= 0; i < recordsArray.Length; i++)
                 {
-                    Console.WriteLine(record.OrderNumber);
-                    Console.WriteLine(record.CustomerNumber);
-                    Console.WriteLine(record.Description);
-                    Console.WriteLine(record.Quantity);                        
+                    csvWriter.WriteField(recordsArray[i].OrderNumber);
+                    csvWriter.WriteField(recordsArray[i].Customer);
+                    csvWriter.WriteField(recordsArray[i].Amount);
+
+                    bool isLastRecord = i == recordsArray.Length - 1;
+
+                    if (!isLastRecord)
+                    {
+                        csvWriter.NextRecord();
+                    }
                 }
+
             }
         }
     }
---
+++ ProcessedOrderMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace DataProcessor
+{
+    class ProcessedOrderMap : ClassMap<ProcessedOrder>
+    {
+        public ProcessedOrderMap()
+        {
+            AutoMap();
+
+            Map(m => m.Customer).Name("CustomerNumber");
+            Map(m => m.Amount).Name("Quantity").TypeConverter<RomanTypeConverter>();
+        }
+    }
+}
+
